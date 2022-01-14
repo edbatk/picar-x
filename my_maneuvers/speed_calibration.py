fe302c68cf57c528e5d5b65d871eccba63bb266c
Ryan <batker@oregonstate.edu>
2022-01-14 11:15:27 -0800

    stuff

--- my_maneuvers/speed_calibration.py
+++ my_maneuvers/speed_calibration.py
@@ -9,6 +9,8 @@ import time
 if __name__ == "__main__":
     try:
         px = Picarx()
+        time.sleep(2.0)
+        px.set_dir_servo_angle(0)
         time.sleep(3.0)
         px.forward(25)
         time.sleep(3.0)
