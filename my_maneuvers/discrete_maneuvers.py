4a80fa7ec180c8b5f3eac962bdf20d8e986713f5
Ryan <batker@oregonstate.edu>
2022-01-14 14:16:51 -0800

    parallel park

--- my_maneuvers/discrete_maneuvers.py
+++ my_maneuvers/discrete_maneuvers.py
@@ -10,7 +10,7 @@ import time
 def linear_movements(px, speed, angle, duration):
     px.stop()
     px.set_dir_servo_angle(angle)
-    time.sleep(1.0)
+    time.sleep(0.5)
     if speed >= 0:
         px.forward(speed)
     else:
@@ -18,17 +18,22 @@ def linear_movements(px, speed, angle, duration):
     time.sleep(duration)
     px.stop()
     
-
-
+def parallel_park(px, side):
+    time.sleep(1.0)
+    if side == 'left':
+        linear_movements(px, -7.5, -25, 1.0)
+        linear_movements(px, 7.5, 50, 0.25)
+        linear_movements(px, 5, 0, 0.25)
+    elif side == 'right':
+        linear_movements(px, -7.5, 25, 1.0)
+        linear_movements(px, 7.5, -50, 0.25)
+        linear_movements(px, 5, 0, 0.25)
+        
 if __name__ == "__main__":
     try:
         px = Picarx()
         time.sleep(1.0)
-        linear_movements(px, 20, 0, 3.0)
-        time.sleep(1.0)
-        linear_movements(px, -20, 0, 2.0)
-        time.sleep(1.0)
-        linear_movements(px, 15, -25, 1.0)
+        parallel_park('left')
           
         
     finally:
