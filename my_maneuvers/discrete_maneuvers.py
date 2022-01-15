96d6983eade4d07e4f464fb60d8115528c1c2108
Ryan <batker@oregonstate.edu>
2022-01-15 10:31:07 -0800

    menu

--- my_maneuvers/discrete_maneuvers.py
+++ my_maneuvers/discrete_maneuvers.py
@@ -44,13 +44,33 @@ def k_turn(px, side):
         
         
 if __name__ == "__main__":
-    try:
-        px = Picarx()
-        time.sleep(1.0)
-        k_turn(px,'left')
-        
-          
-        
-    finally:
-        px.forward(0)
-        px.stop()
+    px = Picarx()
+    curr_move = 'None'
+    disc_maneuvers = ['linear', 'parallel-park', 'k-turn', 'end']
+    sides = ['left', 'right']
+    while curr_move != 'end':
+        curr_move = input('Please select a maneuver from: [linear, parallel-park, k-turn, or end]').lower()
+        print(curr_move)
+        while curr_move not in disc_maneuvers:
+            curr_move = input('Please select a maneuver from the following options: linear, parallel-park, k-turn, or end]').lower()
+        if curr_move == 'end':
+            print('Ending control... Please stand by')
+            time.sleep(1.0)
+            continue
+        elif curr_move == 'linear':
+            speed = float('Please enter a desired speed (-ve for backwards)')
+            angle = float('Please enter a desired turning angle')
+            duration = float('Please enter a desired duration')
+            linear_movements(px, speed, angle, duration)
+        elif curr_move == 'parallel-park':
+            side = input('Please enter the desired direction [left or right]').lower()
+            while side not in sides:
+                side = input('Please enter the desired direction [left or right]').lower()
+            parallel_park(px, side)
+        elif curr_move == 'k-turn':
+            side = input('Please enter the desired direction [left or right]').lower()
+            while side not in sides:
+                side = input('Please enter the desired direction [left or right]').lower()
+            k_turn(px, side)
+    print('Goodbye!')
+            
