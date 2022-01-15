fdb3b5b781ecc91d1c2d8b862b37b6b10137bfda
Ryan <batker@oregonstate.edu>
2022-01-14 18:16:36 -0800

    k turn

--- my_maneuvers/discrete_maneuvers.py
+++ my_maneuvers/discrete_maneuvers.py
@@ -37,7 +37,7 @@ def k_turn(px, side):
     px.set_dir_servo_angle(0)
     if side == 'left':
         linear_movements(px, 7.5, 25, 1.0)
-        linear_movements(px, -7.5, 25, 1.0)
+        linear_movements(px, -7.5, -25, 1.0)
         linear_movements(px, 7.5, -25, 1.0)
         linear_movements(px, 7.5, 0, 1.0)
         px.set_dir_servo_angle(0)
