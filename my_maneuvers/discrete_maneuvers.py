fe7195a1928adf0c5c23b48c50f4e8eb1ce7aca0
Ryan <batker@oregonstate.edu>
2022-01-14 18:15:30 -0800

    k turn

--- my_maneuvers/discrete_maneuvers.py
+++ my_maneuvers/discrete_maneuvers.py
@@ -36,10 +36,10 @@ def k_turn(px, side):
     time.sleep(1.0)
     px.set_dir_servo_angle(0)
     if side == 'left':
-        linear_movements(px, 7.5, 25, 0.4)
-        linear_movements(px, -7.5, 25, 0.4)
-        linear_movements(px, 7.5, -25, 0.4)
-        linear_movements(px, 7.5, 0, 0.4)
+        linear_movements(px, 7.5, 25, 1.0)
+        linear_movements(px, -7.5, 25, 1.0)
+        linear_movements(px, 7.5, -25, 1.0)
+        linear_movements(px, 7.5, 0, 1.0)
         px.set_dir_servo_angle(0)
         
 if __name__ == "__main__":
