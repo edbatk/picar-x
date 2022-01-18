9be92097ffabc6a3204e2b731338db024d11f04a
Ryan <batker@oregonstate.edu>
2022-01-18 09:11:48 -0800

    stuff

--- lib/line_follow.py
+++ lib/line_follow.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append(r'/home/ryan/picar-x/lib')
 from utils import reset_mcu
 reset_mcu()
+import time
 
 from sensing import sensing
 from interpreter import interpreter
@@ -13,10 +14,10 @@ if __name__=='__main__':
     scaling = int(input("Please enter scaling (default 80): "))
     brightness = float(input("Please enter brightness factor (0-1): "))
     polarity = int(input("Please enter polarity (1=light, -1=dark): "))
-    min_speed = float(input("Please enter minimum speed: "))
-    max_speed = float(input("Please enter max speed: "))
+    # min_speed = float(input("Please enter minimum speed: "))
+    # max_speed = float(input("Please enter max speed: "))
     sensor = sensing()
-    interpreter = interpreter(0.0, polarity)
+    interpreter = interpreter(brightness, polarity)
     controller = line_follow_controller(scaling)
       
     while True:
@@ -25,5 +26,7 @@ if __name__=='__main__':
         set_angle = controller.control(px,direction)
         # speed = (max_speed - min_speed)*(abs(direction)-1.0)/-0.1
         speed = 1.0
-        px.forward(speed)
+        # px.forward(speed)
+        time.sleep(0.1)
+        
             
