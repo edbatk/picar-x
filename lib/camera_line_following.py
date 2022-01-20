4658ea4f677bad0647c284d09bc6eb92d17f49af
Ryan <batker@oregonstate.edu>
2022-01-20 14:36:50 -0800

    camera stuff

--- lib/camera_line_following.py
+++ lib/camera_line_following.py
@@ -283,7 +283,7 @@ class HandCodedLaneFollower(object):
         new_steering_angle = compute_steering_angle(frame, lane_lines)
         self.curr_steering_angle = stabilize_steering_angle(self.curr_steering_angle, new_steering_angle, len(lane_lines))
 
-        if self.car is not None:
+        if self.px is not None:
             self.px.set_dir_servo_angle(self.curr_steering_angle)
             self.forward(30)
         curr_heading_image = display_heading_line(frame, self.curr_steering_angle)
